d7bbe548742dd2ce3694516ce977c3437397fa85
isuru kularathna <isurujr.online@gmail.com>
2023-11-28 21:54:15 +0530

    configure postgreSQL db connection

--- src/main/java/com/isuruk/dev/app/QuickstartJava17Springboot3Application.java
+++ src/main/java/com/isuruk/dev/app/QuickstartJava17Springboot3Application.java
@@ -1,16 +1,26 @@
 package com.isuruk.dev.app;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Slf4j
 @SpringBootApplication
 public class QuickstartJava17Springboot3Application {
 
+	@Autowired
+	DataSource dataSource;
 
+	@Bean
+	public JdbcTemplate jdbcTemplate(){
+		return new JdbcTemplate(dataSource);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickstartJava17Springboot3Application.class, args);
@@ -19,7 +29,8 @@ public class QuickstartJava17Springboot3Application {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-
+			log.info("Datasource : {}",dataSource.toString());
+			jdbcTemplate().execute("SELECT 1");
 		};
 	}
 
